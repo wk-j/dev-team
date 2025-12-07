7a19262c9b9f95781bd10779365d4d436a8ff5c9
wk <somnuk.wk@gmail.com>
2025-12-08 01:46:05 +0700

    fix: add trustHost for NextAuth on Vercel

--- app/src/lib/auth/index.ts
+++ app/src/lib/auth/index.ts
@@ -13,6 +13,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
+  trustHost: true,
   pages: {
     signIn: "/login",
     newUser: "/onboarding",
